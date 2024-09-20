717f6a062cb087b6c4ea9bccbd70ff463a0a5d81
bhaskardixit <bhaskar.dixit@xebia.com>
2024-09-20 18:53:47 +0530

    binary search

---
+++ src/concepts/searching/DemoBinarySearch.java
@@ -0,0 +1,30 @@
+package concepts.searching;
+
+import java.util.Arrays;
+
+public class DemoBinarySearch {
+	
+//	static void binarySearch(int arr[], int first, int last, int key) {
+//		int mid = (first+last)/2;
+//		while(first<=last) {
+//			if(arr[mid]<key) {
+//				first = mid +1;
+//			} else if (arr[mid] == key) {
+//				System.out.println("element found "+ arr[mid]);
+//			} else {
+//				last = mid -1;
+//			}
+//			mid = (first+last)/2;
+//		}
+//	}
+//	
+	public static void main(String[] args) {
+//		int arr[]  =  {2,4,5,7,8};
+//		Arrays.sort(arr);
+//		
+		System.out.println("Runnin binary search");
+//		
+//		binarySearch(arr, 0 , arr.length, 5);
+		
+	}
+}
